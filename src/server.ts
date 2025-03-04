f1171d7bf1088ae11fc8b26f716419718d02b745
Srinivas-LV50 <srinivas.gollapalli@launchventures.co>
2025-03-04 17:20:30 +0530

    vercel port escape

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 // src/server.ts
 import app from "./app";
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (!process.env.VERCEL) {
+  // Only start the server locally
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
