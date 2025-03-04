03ab4350296d2e338d7da4f20998b2be8b1ded7f
Srinivas-LV50 <srinivas.gollapalli@launchventures.co>
2025-03-05 02:15:14 +0530

    again testing

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@
 import app from "./app";
 
 if (!process.env.VERCEL) {
-  // Only start the server locall
+  // Only start the server local
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
